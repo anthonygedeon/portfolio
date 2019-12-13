e3f2b2b814d935dd0ed055c633db33cd20a3f255
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-12-13 05:34:12 -0600

    positioned the menu out at the bottom since it was not getting it's full with by being inside the nav element

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,10 +2,10 @@ const menu = document.querySelector('.navigation__menu');
 const menuOut = document.querySelector('.navigation__menu--show');
 
 function renderMenu(menu) {
-    if (menu.style.display != 'none') {
-        menu.style.display = 'none'
-    } else {
+    if (menu.style.display == 'none') {
         menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
     }
 }
 
