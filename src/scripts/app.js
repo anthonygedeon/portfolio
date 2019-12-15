1a7c63f5ae0f75ae3215daa74c329f32e212a4b4
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-12-14 19:45:41 -0600

    navbar for links displays correctly

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,11 +1,20 @@
 const menu = document.querySelector('.navigation__menu');
 const menuOut = document.querySelector('.navigation__menu--show');
 
+function noScroll() {
+    window.scrollTo(0,0);
+}
+
 function renderMenu(menu) {
-    if (menu.style.display == 'none') {
-        menu.style.display = 'block'
-    } else {
+    if (menu.style.display == 'block') {
         menu.style.display = 'none'
+        window.removeEventListener('scroll', noScroll);
+
+    } else {
+        menu.style.display = 'block'
+        window.addEventListener('scroll', noScroll);
+
+        
     }
 }
 
