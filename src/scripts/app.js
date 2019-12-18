6521c57b402d90c286347b81dcdf5644d238c889
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-12-17 20:56:10 -0600

    worked on responsive navbar and finally fixed the logo

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,22 +1,8 @@
-const menu = document.querySelector('.navigation__menu');
-const menuOut = document.querySelector('.navigation__menu--show');
+const nav = document.querySelector('.nav')
+const menu = document.querySelector('.nav__menu');
 
-function noScroll() {
-    window.scrollTo(0,0);
+function changeColor() {
+    nav.style.backgroundColor = '#121212';
 }
 
-function renderMenu(menu) {
-    if (menu.style.display == 'block') {
-        menu.style.display = 'none'
-        window.removeEventListener('scroll', noScroll);
-
-    } else {
-        menu.style.display = 'block'
-        window.addEventListener('scroll', noScroll);
-    }
-}
-
-menu.addEventListener('click', () => {
-    renderMenu(menuOut);
-});
-
+menu.addEventListener('click', changeColor);
