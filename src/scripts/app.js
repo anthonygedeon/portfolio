b0a40bbcd9a368e9d8821cb1195d1137b70ce66c
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-12-19 20:17:25 -0600

    responsive homepage

--- src/scripts/app.js
+++
@@ -1,8 +0,0 @@
-const nav = document.querySelector('.nav')
-const menu = document.querySelector('.nav__menu');
-
-function changeColor() {
-    nav.style.backgroundColor = '#121212';
-}
-
-menu.addEventListener('click', changeColor);
