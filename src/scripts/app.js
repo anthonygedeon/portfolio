6446b206d8cdecd80d98e71251505d02ca38c0d9
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-12-21 04:05:21 -0600

    still side scrolling, chanign to -10opx

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,8 @@
 const menuBar = document.querySelector('.home__nav-bar');
 
 function slideOut() {
-    const bar = document.querySelector('.home__nav-anime')
-    bar.style.left = '-8';
+    const bar = document.querySelector('.home__nav-anime');
+    bar.style.left = '-10px';
 }
 
 menuBar.addEventListener('click', slideOut);
