009a06a54d664c1ae40683afd4b3b1a89610762c
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-12-21 03:33:48 -0600

    added slide out for navbar

---
+++ src/scripts/app.js
@@ -0,0 +1,9 @@
+const menuBar = document.querySelector('.home__nav-bar');
+
+function slideOut() {
+    const bar = document.querySelector('.home__nav-anime')
+    bar.style.left = '0';
+}
+
+menuBar.addEventListener('click', slideOut);
+menuBar.addEventListener('touch', slideOut);
