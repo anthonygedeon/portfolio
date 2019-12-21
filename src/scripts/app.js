5a0cd5d33c611ff4caf5a5d7edce53a6e60a56cb
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-12-21 04:01:23 -0600

    on mobile it was side scrolling because of the position of the slide out

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,7 +2,7 @@ const menuBar = document.querySelector('.home__nav-bar');
 
 function slideOut() {
     const bar = document.querySelector('.home__nav-anime')
-    bar.style.left = '0';
+    bar.style.left = '-8';
 }
 
 menuBar.addEventListener('click', slideOut);
